2a1c350d933edffa5276edc16b640bfbc26114de
leonardo <leonardo.quinino@gmail.com>
2022-07-11 22:02:33 -0300

    Projeto Teste para Mostrar o Funcionamento do StringBuilder

---
+++ TestandoStringBuilder/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestandoStringBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btProcessar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(122, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 164);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Teste 1\nTeste 2\nTeste 3\nTeste 4\nTeste 5";
+            // 
+            // btProcessar
+            // 
+            this.btProcessar.Location = new System.Drawing.Point(334, 218);
+            this.btProcessar.Name = "btProcessar";
+            this.btProcessar.Size = new System.Drawing.Size(153, 66);
+            this.btProcessar.TabIndex = 1;
+            this.btProcessar.Text = " Validar";
+            this.btProcessar.UseVisualStyleBackColor = true;
+            this.btProcessar.Click += new System.EventHandler(this.btProcessar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btProcessar);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button btProcessar;
+    }
+}
